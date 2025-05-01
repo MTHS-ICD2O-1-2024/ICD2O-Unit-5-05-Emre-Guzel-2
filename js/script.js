fe6d85008867bb5d31a104b192d256e2f25c5b80
default <yunus.emre.guzel@mths.ca>
2025-05-01 13:07:27 +0000

    finshed the forntend

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: April 29 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+// Setting the functoin 
+function musuemDiscountAgeCalculator() {
+  // Setting the variables
+  const age = parseInt(document.getElementById("age").value)
+  const day = document.getElementById("day").value
+  let message = ""
+
+  // Setting the if statement
+  if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21)){
+    message = "You're eligible for student pricing";
+  } else {
+    message = "You must pay the regular pricing"
+  }
+
+  document.getElementById("result").innerHTML = message
+}
